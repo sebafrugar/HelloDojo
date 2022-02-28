82d47809812eb4f423c04ea84cfeefb0eb21baad
sebafrugar <sebafrugar@gmail.com>
2022-02-28 19:18:09 -0400

    Primeros pasos en React

---
+++ src/components/ToDo.js
@@ -0,0 +1,20 @@
+import React, { Component} from 'react';
+
+class ToDo extends Component{
+    render(){
+        return(
+            <div className="Aprendiendo">
+                <h1>Hello Dojo!</h1>
+                <h4>Things I need to do :</h4>
+                <ul>
+                    <li>Learn React</li>
+                    <li>Climb Mt. Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default ToDo;
